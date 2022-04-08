2afa891d6ae2060de4eafb992b378974a191ef7a
Michel Plainchamp <michel.plainchamp@gmail.com>
2022-04-08 09:27:24 +0200

    edited the javascript

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-const someHTMLCollection = document.querySelectorAll('.col-sm-6')
+// const someHTMLCollection = document.querySelectorAll('.col-sm-6')
+const someHTMLCollection = document.querySelectorAll('.container div')
+
+
+
+
+
+Array.from(someHTMLCollection).forEach((elt, index) => {
+	console.log(`\nElement (${index}) ----------------------\ndiv content : \n\n`, elt.innerText)
+})
 
 
 console.log(someHTMLCollection)
